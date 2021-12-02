6fe859cfe3736963b1ec83443455019099ef95b2
ao1115 <1677120510@qq.com>
2021-12-02 18:16:53 +0800

    添加grid布局

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -9,6 +9,13 @@ export const Home = () => {
         <h2>国家公安合成作战平台</h2>
         <span>2021年12月2日 12:22:00</span>
       </header>
+      <main>
+        <section className="section1">1111</section>
+        <section className="section2">222</section>
+        <section className="section3">3</section>
+        <section className="section4">4</section>
+        <section className="section5">5</section>
+      </main>
     </div>
   );
 };
