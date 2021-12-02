cf62d2bba99c26934c8664a15a1b93215803efdc
ao1115 <1677120510@qq.com>
2021-12-02 19:26:20 +0800

    重构Chart1

---
+++ src/components/chart-1.tsx
@@ -0,0 +1,62 @@
+import React, { useRef, useEffect } from 'react';
+import * as echarts from 'echarts'
+const px = (n) => n / 2420 * (window as any).pageWidth;
+
+export const Chart1 = () => {
+    const divRef = useRef(null)
+    useEffect(() => {
+        var myChart = echarts.init(divRef.current);
+        myChart.setOption({
+            textStyle: {
+                fontSize: px(12),
+                color: '#79839E'
+            },
+            title: { show: false },
+            legend: { show: false },
+            xAxis: {
+                data: ['北京', '上海', '重庆', '天津', '内蒙古', '新疆', '甘肃', '陕西', '山西'],
+                axisTick: { show: false },
+                axisLine: {
+                    lineStyle: { color: '#083B70' }
+                },
+                axisLabel: {
+                    fontSize: px(12),
+                    formatter(val) {
+                        if (val.length > 2) {
+                            const array = val.split('');
+                            array.splice(2, 0, '\n');
+                            return array.join('');
+                        } else {
+                            return val;
+                        }
+                    }
+                },
+            },
+            grid: {
+                x: px(40),
+                y: px(40),
+                x2: px(40),
+                y2: px(40),
+            },
+            yAxis: {
+                splitLine: { show: false },
+                axisLine: {
+                    show: true,
+                    lineStyle: { color: '#083B70' }
+                },
+                axisLabel: {
+                    fontSize: px(12)
+                }
+            },
+            series: [{
+                type: 'bar',
+                data: [10, 20, 36, 41, 15, 26, 37, 18, 29]
+            }]
+        });
+    }, []);
+    return (
+        <div className="chart" ref={divRef}>
+        </div>
+    )
+
+}
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,61 +1,10 @@
 import React, { useRef, useEffect } from 'react';
 import './home.scss';
 import headerBg from "../images/header.png"
-import * as echarts from 'echarts'
-const px = (n) => n / 2420 * (window as any).pageWidth;
+import { Chart1 } from '../components/chart-1';
 
 export const Home = () => {
-  const divRef = useRef(null)
-  useEffect(() => {
-    var myChart = echarts.init(divRef.current);
-    myChart.setOption({
-      textStyle: {
-        fontSize: px(12),
-        color: '#79839E'
-      },
-      title: { show: false },
-      legend: { show: false },
-      xAxis: {
-        data: ['北京', '上海', '重庆', '天津', '内蒙古', '新疆', '甘肃', '陕西', '山西'],
-        axisTick: { show: false },
-        axisLine: {
-          lineStyle: { color: '#083B70' }
-        },
-        axisLabel: {
-          fontSize: px(12),
-          formatter(val) {
-            if (val.length > 2) {
-              const array = val.split('');
-              array.splice(2, 0, '\n');
-              return array.join('');
-            } else {
-              return val;
-            }
-          }
-        },
-      },
-      grid: {
-        x: px(40),
-        y: px(40),
-        x2: px(40),
-        y2: px(40),
-      },
-      yAxis: {
-        splitLine: { show: false },
-        axisLine: {
-          show: true,
-          lineStyle: { color: '#083B70' }
-        },
-        axisLabel: {
-          fontSize: px(12)
-        }
-      },
-      series: [{
-        type: 'bar',
-        data: [10, 20, 36, 41, 15, 26, 37, 18, 29]
-      }]
-    });
-  }, []);
+
   return (
     <div className="home" >
       <header className="header" style={{ backgroundImage: `url(${headerBg})` }}>
@@ -66,8 +15,7 @@ export const Home = () => {
         <section className="section1 ">
           <div className="管辖统计 borderd">
             <h2>案发派出所管辖统计</h2>
-            <div className="chart" ref={divRef}>
-            </div>
+            <Chart1 />
           </div>
         </section>
         <section className="section2  borderd" >222</section>
