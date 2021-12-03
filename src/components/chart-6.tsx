7c74b7c218678c90d0636ae7c305df94cb0f3310
ao1115 <1677120510@qq.com>
2021-12-03 13:45:39 +0800

    解决引入地图的JSON文件报错问题

--- src/components/chart-6.tsx
+++ src/components/chart-6.tsx
@@ -1,13 +1,70 @@
 import React, { useRef, useEffect } from 'react';
 import * as echarts from 'echarts'
 import { px } from '../shared/px';
-
+import china from '../geo/china.json';
 export const Chart6 = () => {
     const divRef = useRef(null)
     const colors = { '青海省': '#BB31F7', '甘肃省': '#15B8FD', '四川省': '#06E1EE' };
     useEffect(() => {
         var myChart = echarts.init(divRef.current);
-        myChart.setOption({})
+        //@ts-ignore
+        echarts.registerMap('CN', china);
+        myChart.setOption({
+            xAxis: { show: false },
+            yAxis: { show: false },
+            series: [
+                {
+                    type: 'map',
+                    mapType: 'CN', // 自定义扩展图表类型
+                    data: [
+                        { name: '甘肃省', value: 1 },
+                    ],
+                    label: { show: false, color: 'white' },
+                    itemStyle: {
+                        areaColor: '#010D3D',
+                        color: colors['甘肃省'],
+                        borderColor: '#01A7F7',
+                        emphasis: {
+                            label: { color: 'white' },
+                            areaColor: '#5470C6',
+                        },
+                    }
+                },
+                {
+                    type: 'map',
+                    mapType: 'CN', // 自定义扩展图表类型
+                    data: [
+                        { name: '四川省', value: 100 },
+                    ],
+                    itemStyle: {
+                        areaColor: '#010D3D',
+                        color: colors['四川省'],
+                        borderColor: 'yellow',
+                        emphasis: {
+                            label: { color: 'white' },
+                            areaColor: '#5470C6',
+                        },
+                    }
+                },
+                {
+                    type: 'map',
+                    mapType: 'CN', // 自定义扩展图表类型
+                    data: [
+                        { name: '青海省', value: 100 },
+                    ],
+                    itemStyle: {
+                        areaColor: '#010D3D',
+                        color: colors['青海省'],
+                        borderColor: '#01A7F7',
+                        emphasis: {
+                            label: { color: 'white' },
+                            areaColor: '#5470C6',
+                        },
+                    }
+                },
+
+            ]
+        })
     }, [])
     return (
         <div className="地图 bordered">
