f30254dc2cdd0e176051422622f7c316c8c591c0
ao1115 <1677120510@qq.com>
2021-12-03 16:32:29 +0800

    完成Chart11动态显示

--- src/components/chart-11.tsx
+++ src/components/chart-11.tsx
@@ -4,6 +4,7 @@ import { px } from '../shared/px';
 
 export const Chart11 = () => {
     const divRef = useRef(null)
+    const myChart = useRef(null)
     const colors = ['#F46064', '#F38E1C', '#1CDB7C', '#8D70F8', '#33A4FA'];
     const data = [
         { value: 0.08, name: '北京' },
@@ -12,39 +13,44 @@ export const Chart11 = () => {
         { value: 0.09, name: '重庆' },
         { value: 0.12, name: '呼和浩特' },
         { value: 0.06, name: '乌鲁木齐' },
-        { value: 0.08, name: '北京' },
-        { value: 0.06, name: '上海' },
-        { value: 0.11, name: '天津' },
-        { value: 0.09, name: '重庆' },
-        { value: 0.12, name: '呼和浩特' },
-        { value: 0.06, name: '乌鲁木齐' },
+        { value: 0.06, name: '长春' },
+        { value: 0.06, name: '沈阳' },
+        { value: 0.04, name: '黑龙江' },
+        { value: 0.10, name: '西安' },
+        { value: 0.12, name: '石家庄' },
+        { value: 0.06, name: '济南' },
+        { value: 0.09, name: '青岛' },
+        { value: 0.12, name: '成都' },
+        { value: 0.03, name: '深圳' },
+        { value: 0.12, name: '广州' },
+        { value: 0.06, name: '海南' },
+        { value: 0.12, name: '武汉' },
+        { value: 0.06, name: '南京' },
     ];
-    // let arrayShow = [
-    //     { value: 0.08, name: '北京' },
-    //     { value: 0.06, name: '上海' },
-    //     { value: 0.11, name: '天津' },
-    //     { value: 0.09, name: '重庆' },
-    //     { value: 0.12, name: '呼和浩特' },
-    // ];
-    // let index = 0;
-    // const change = () => {
-    //     if (index >= 6) {
-    //         index = 0
-    //     }
-    //     for (let i = 0; i < data.length; i++) {
-    //         arrayShow = data.slice(index + 5, index + 10)
-
-    //     }
-    //     index = index + 1
-    //     console.log(index)
-    // }
-
+    let arrayShow = [];
+    let index = 0;
+    const change = () => {
+        if (index >= 6) {
+            index = 0
+        }
+        for (let i = 0; i < data.length; i++) {
+            arrayShow = data.slice(index, index + 5)
+            if (arrayShow.length < 5) {
+                arrayShow = data.slice(1, 5)
+            }
+        }
+        index = index + 1
+        console.log(index)
+    }
     useEffect(() => {
+        setInterval(() => {
+            change()
+            X(arrayShow)
+            console.log(arrayShow)
+        }, 3000)
+    })
+    function X(data) {
         var myChart = echarts.init(divRef.current);
-        // setInterval(() => {
-        //     change()
-        //     console.log(arrayShow)
-        // }, 1000)
         myChart.setOption({
             textStyle: {
                 fontSize: px(18),
@@ -84,7 +90,7 @@ export const Chart11 = () => {
                 }
             ]
         });
-    }, []);
+    }
     return (
         <div className="案发地级市  bordered">
             <h2>案发地级市统计</h2>
